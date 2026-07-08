e7fd15d9ea07672a38500f5e46b23aaac116723d
Bhavin Jethva <jethava.bhavin@gmail.com>
2026-07-08 18:20:10 +0530

    Run both app together

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+  { command: "bun run apps/server/index.ts", name: "server", cwd: "apps/server", prefixColor: "cyan" },
+  { command: "bun run apps/pwa/index.ts", name: "pwa", cwd: "apps/pwa", prefixColor: "green" },
+]);
