e3ac0483c86c9abfcf8b89915744970a0975f7fb
Bhavin Jethva <jethava.bhavin@gmail.com>
2026-07-08 19:14:23 +0530

    Fix Issue

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 import concurrently from "concurrently";
 
 concurrently([
-  { command: "bun run apps/server/index.ts", name: "server", cwd: "apps/server", prefixColor: "cyan" },
-  { command: "bun run apps/pwa/index.ts", name: "pwa", cwd: "apps/pwa", prefixColor: "green" },
+  { 
+    command: "bun run dev", 
+    name: "server", 
+    cwd: "apps/server", 
+    prefixColor: "cyan" 
+  },
+  { 
+    command: "bun run dev", 
+    name: "pwa", 
+    cwd: "apps/pwa", 
+    prefixColor: "green" 
+  }
 ]);
