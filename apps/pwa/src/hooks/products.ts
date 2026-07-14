d8d7314cdb106a015c7ea5ddde4ff4207acf939c
Bhavin Jethva <jethava.bhavin@gmail.com>
2026-07-14 23:02:10 +0530

    Add env config with react

--- apps/pwa/src/hooks/products.ts
+++ apps/pwa/src/hooks/products.ts
@@ -1,9 +1,10 @@
 // hooks/useProducts.ts
 import { useQuery } from '@tanstack/react-query'
 import type Product from '@/type/product'
+import { env } from '@/config/env'
 
 async function fetchProducts(): Promise<{ data: Product[] }> {
-   const res = await fetch('/api/products')
+   const res = await fetch(env.apiUrl + '/api/products')
    if (!res.ok) {
       throw new Error('Failed to fetch products')
    }
